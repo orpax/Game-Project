9e1fade28e6539397ccb22ebc6d9b3aea869c548
Tayoo <120953014+orpax@users.noreply.github.com>
2024-04-17 16:08:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 0.2
+
 class sprite {
-    constructor(position, velocity) {
+    constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
+        this.height = 150
     }
 
     draw(){
-        c.fillStyle = "red"
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillStyle = "red";
+        c.fillRect(this.position.x, this.position.y, 50, this.height);
+    }
+
+    update(){
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+    if(this.position.y  + this.height + this.velocity.y >= canvas.height){
+        this.velocity.y = 0;
+    }else this.velocity.y += gravity
     }
 }
 
@@ -36,9 +49,51 @@ enemy.draw()
 
 console.log(player)
 
+const keys = {
+    a : {
+        pressed : false,
+    },
+    d : {
+        pressed : false,
+    }
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
-    console.log("k")
+    c.fillStyle = "black"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+if (key.a.pressed) {
+    player.velocity.x = -1
+}else if (key.d.pressed) {
+    player.velocity.x = 1
 }
 
 animate()
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "d":
+            player.velocity.x = 1
+            break
+        case "a":
+            player.velocity.x = -1
+            break
+    }
+    console.log(event.key)
+});
+
+window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+        case "d":
+            player.velocity.x = 0
+            break
+        case "a":
+            player.velocity.x = 0
+            break
+    }
+    console.log(event.key)
+});
